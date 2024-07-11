ef0a13bae650f2ddbe9e756456cfd2123992bdc0
franserra <franserra11@gmail.com>
2024-07-11 18:05:46 -0300

    second commit e-commerce proyect entrega 1

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-/* CALCULATOR */
-
-const display = document.getElementById('display');
-
-function appendToDisplay(input){
-    display.value += input;
-}
-
-function clearDisplay(){
-    display.value = '';
-}
-
-function calculate(){
-    try{
-        display.value = eval(display.value);
-    } catch(error){
-        display.value = 'Error';
-    }
-}
-
 /* E-COMMERCE */
 
 document.addEventListener('DOMContentLoaded', () => {
